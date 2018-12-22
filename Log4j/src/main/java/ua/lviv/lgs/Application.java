1e02ab67a3849905238e348715c56b4881d1ecae
Roman <romanbr@meta.ua>
2018-12-22 21:19:28 +0200

    Update Application.java

--- Log4j/src/main/java/ua/lviv/lgs/Application.java
+++ Log4j/src/main/java/ua/lviv/lgs/Application.java
@@ -20,38 +20,15 @@ public class Application {
 		
 		MagazineDao magazineDao = new MagazineDao(ConnectionUtils.openConnection());
 		UserDao userDao = new UserDao(ConnectionUtils.openConnection());
+		
+		
 		// READ ALL
 		System.out.println("---- MAGAZINES");
 		magazineDao.readAll().stream().forEach(System.out::println);
 		System.out.println("---- USERS");
 		userDao.readAll().stream().forEach(System.out::println);
 		System.out.println("**********************************************************");
-//		List<Magazine> listOfMagazine = new ArrayList<>();
-//		listOfMagazine.add(new Magazine("Test drive", "Auto"));
-//		listOfMagazine.add(new Magazine("Country secrets", "Garden"));
-//		listOfMagazine.add(new Magazine("Mens Fitness", "Sport"));
-//		listOfMagazine.add(new Magazine("4*4", "Auto"));
-//		listOfMagazine.add(new Magazine("Country secrets #2", "Garden"));
-//		listOfMagazine.add(new Magazine("Iron world", "Sport"));
-		// INSERT Magazines
-//		listOfMagazine.forEach(employee->{
-//			try {
-//				magazineDao.insert(employee);
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//		});
-//		System.out.println("**********************************************************");
-		// INSERT Users
-//		List<User> listOfUser = new ArrayList<>();
-//		listOfUser.add(new User("Mike", "Smith"));
-//		listOfUser.forEach(user->{
-//			try {
-//				userDao.insert(user);
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//		});
+
 
 //		READ BY ID
 		Magazine magazineFromDB = magazineDao.read(2);
@@ -67,8 +44,9 @@ public class Application {
 //		READ ALL
 		magazineDao.readAll().stream().forEach(System.out::println);
 		System.out.println("**********************************************************");
-		System.out.println("**********************************************************");
-		System.out.println("**********************************************************");
+		
+		
+		
 //		READ BY ID
 		User userFromDB = userDao.read(2);
 		System.out.println(userFromDB);
